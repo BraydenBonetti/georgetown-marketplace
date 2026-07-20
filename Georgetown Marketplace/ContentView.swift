1237781a0beb39151ac87ce7acd8607c2eba8af3
Brayden Bonetti <brayden@bonettifamily.com>
2026-07-20 14:40:36 -0400

    Initial Commit

---
+++ Georgetown Marketplace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Georgetown Marketplace
+//
+//  Created by Brayden Bonetti on 7/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Georgetown Marketplace/Georgetown_MarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Georgetown_MarketplaceApp.swift
+//  Georgetown Marketplace
+//
+//  Created by Brayden Bonetti on 7/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Georgetown_MarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
